9a393e4c00d03203cbad92bc11446f7fc6668af3
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-11-06 21:50:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -64,7 +64,19 @@ def game_hash
     :away => {
       :team_name => "Charlotte Hornets",
       :colors => ["Torquoise", "Purple"],
-      :players => [{}]
+      :players => [
+        {
+          :player_name => "Jason Terry",
+          :number => 31,
+          :shoe => 15,
+          :points => 19,
+          :rebounds => 2,
+          :assists => 2,
+          :steals => 4,
+          :blocks => 11,
+          :slam_dunks => 1
+        }
+        ]
     }
   }
 end
