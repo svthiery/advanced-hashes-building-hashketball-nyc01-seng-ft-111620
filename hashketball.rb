9c374341eb27723280c7942df7636939898a2c70
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-11-06 21:51:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -75,6 +75,17 @@ def game_hash
           :steals => 2,
           :blocks => 7,
           :slam_dunks => 2
+        },
+        {
+          :player_name => "Jeff Adrien",
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2
         }
         ]
     }
