17d5e58dd53941138dcd5ae82c1d5b857708f259
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-11-06 21:45:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,14 +6,25 @@ def game_hash
       :players => [
         {
           :player_name => "Alan Anderson",
-          :number => "0",
-          :shoe => "16",
-          :points => "22",
-          :rebounds => "12",
-          :assists => "12",
-          :steals => "3",
-          :blocks => "1",
-          :slam_dunks => "1"
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
+        },
+        {
+          :player_name => "Alan Anderson",
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
         }
         ]
     },
