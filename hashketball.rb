e26e554d434657730453df8f9c8ebc707210139d
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-11-06 21:47:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -38,15 +38,15 @@ def game_hash
           :slam_dunks => 15
         },
         {
-          :player_name => "Brook Lopez",
-          :number => 11,
-          :shoe => 17,
-          :points => 17,
-          :rebounds => 19,
-          :assists => 10,
+          :player_name => "Mason Plumlee",
+          :number => 1,
+          :shoe => 19,
+          :points => 26,
+          :rebounds => 11,
+          :assists => 6,
           :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 15
+          :blocks => 8,
+          :slam_dunks => 5
         }
         ]
     },
