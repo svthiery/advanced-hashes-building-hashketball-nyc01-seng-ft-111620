19861e1e88ab8ea3775b55e74f02aba3249cdb8d
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-11-06 21:52:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -77,14 +77,14 @@ def game_hash
           :slam_dunks => 2
         },
         {
-          :player_name => "Jeff Adrien",
-          :number => 4,
-          :shoe => 18,
-          :points => 10,
-          :rebounds => 1,
-          :assists => 1,
-          :steals => 2,
-          :blocks => 7,
+          :player_name => "Bismack Biyombo",
+          :number => 0,
+          :shoe => 16,
+          :points => 12,
+          :rebounds => 4,
+          :assists => 7,
+          :steals => 22,
+          :blocks => 15,
           :slam_dunks => 2
         }
         ]
