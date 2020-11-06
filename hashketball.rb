27a4b9ec2a2d4d340fc9c018225e2e47b39cecef
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-11-06 21:54:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -106,8 +106,19 @@ def game_hash
           :rebounds => 3,
           :assists => 2,
           :steals => 1,
-          :blocks => 5,
-          :slam_dunks => 5 
+          :blocks => 1,
+          :slam_dunks => 0 
+        },
+        {
+          :player_name => "Ben Gordon",
+          :number => 8,
+          :shoe => 15,
+          :points => 33,
+          :rebounds => 3,
+          :assists => 2,
+          :steals => 1,
+          :blocks => 1,
+          :slam_dunks => 0 
         }
         ]
     }
