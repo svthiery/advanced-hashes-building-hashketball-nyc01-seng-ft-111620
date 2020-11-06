50d01c5bf1516054af48caa0868c66c21b33c88a
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-11-06 21:46:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -23,8 +23,19 @@ def game_hash
           :rebounds => 12,
           :assists => 12,
           :steals => 12,
-          :blocks => 1,
-          :slam_dunks => 1
+          :blocks => 12,
+          :slam_dunks => 7
+        },
+        {
+          :player_name => "Reggie Evans",
+          :number => 30,
+          :shoe => 14,
+          :points => 12,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 12,
+          :blocks => 12,
+          :slam_dunks => 7
         }
         ]
     },
